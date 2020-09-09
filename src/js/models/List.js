01f81ea57584febc86d37ae8049272b812f888c9
MelBiggs <48949472+MelBiggs@users.noreply.github.com>
2020-09-09 15:32:53 +0100

    Shopping list view and model

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -12,5 +12,20 @@ export default class List {
             unit,
             ingredient
         }
+        this.items.push(item);
+        return item;
+    }
+
+    deleteItem (id) {
+        const index = this.items.findIndex(el => el.id === id);
+
+        // [2,4,8] splice(1,1)-> return [4], original array is [2,8]
+        // [2,4,8] slice(1,1)-> return [4], original array is [2,4,8]
+
+        this.items.splice(index, 1);
+    }
+
+    updateCount(id, newCount) {
+        this.items.find(el => el.id === id).count = newCount;
     }
 }
