f505a26cd1befc894fb9fc6f3329912d76678726
MelBiggs <48949472+MelBiggs@users.noreply.github.com>
2020-09-10 15:21:14 +0100

    Persist like data in localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -154,10 +154,6 @@ elements.searchResPages.addEventListener('click', e => {
  * LIKE CONTROLLER
  */
 
- //Testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -192,6 +188,19 @@ const controlLike = () => {
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes ();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
  // Handling all recipe button clicks
 
